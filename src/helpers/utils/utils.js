356864a22d18bd639152222dfe7d770e8bff360d
timilehin <joeltimmy7@gmail.com>
2022-04-04 17:06:07 +0100

    fixed typo error

--- src/helpers/utils/utils.js
+++ src/helpers/utils/utils.js
@@ -16,6 +16,58 @@ class Utils {
             }
         );
     }
+
+    static async initializeTransaction(params) {
+        let APIUrl = "https://api.paystack.co/transaction/initialize";
+
+        let requestHeaders = {
+            "Content-Type": "application/json",
+        };
+
+        const getResponse = async () => {
+            const { isError, data, errorMessage } = await fetchApi(
+                params,
+                "POST",
+                APIUrl,
+                requestHeaders
+            );
+
+            if (isError) {
+                return { errorMessage, isError };
+            } else {
+                return { data: data.data.data };
+            }
+        };
+
+        const response = await getResponse();
+        return response;
+    }
+
+    static async verifyTransaction(referenceId) {
+        let APIUrl = `https://api.paystack.co/transaction/verify/:reference`;
+
+        let requestHeaders = {
+            "Content-Type": "application/json",
+        };
+
+        const getResponse = async () => {
+            const { isError, data, errorMessage } = await fetchApi(
+                { referenceId },
+                "POST",
+                APIUrl,
+                requestHeaders
+            );
+
+            if (isError) {
+                return { errorMessage, isError };
+            } else {
+                return { data: data.data.data };
+            }
+        };
+
+        const response = await getResponse();
+        return response;
+    }
 }
 
 module.exports = Utils;
